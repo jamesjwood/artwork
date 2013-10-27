f367d5524f7169b2755c3a36eef01607894f4354
jamesjwood <jamesjwood@msn.com>
2013-10-27 17:48:14 +0000

    initial commit

---
+++ gruntfile.js
@@ -0,0 +1,44 @@
+
+var sizes = [29,40,57,60,50,72,76, 58, 80,114,120,100,144,152];
+var shell = {
+  stage:{
+        command: 'rm -R stage; mkdir stage;',
+        stdout: true,
+        stderr: true,
+        failOnError: true
+  }
+};
+var tasks = ['shell:stage'];
+
+sizes.map(function(size){
+  var command = 'qlmanage -t -s '+ size.toString() +' -o ./stage/ icon.svg; mv stage/icon.svg.png stage/icon'+ size.toString() +'.png';
+  shell['i' +size.toString()] = {
+        command: command,
+        stdout: true,
+        stderr: true,
+        failOnError: true
+  };
+  tasks.push('shell:i' + size.toString());
+});
+
+module.exports = function(grunt) {
+  "use strict";
+  grunt.initConfig({
+    watch: {
+      scripts: {
+        files: ['./icon.svg'],
+        tasks: ['default'],
+        options: {
+          spawn: false,
+        }
+      }
+    },
+    shell: shell
+  });
+
+
+
+grunt.loadNpmTasks('grunt-shell');
+
+grunt.registerTask('default', tasks);
+};
